bce99ba05b6cc02dd8615941fd6c4c1ab3a6eccb
Anjali Dewari <anjalidewari19@gmail.com>
2026-08-04 20:52:32 +0530

    Find Missing Element

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -4,6 +4,7 @@ public:
         sort(v.begin(),v.end());
         int n = v.size();
         vector<int> ans;
+        // pushing remaining elements between minimun and maximun 
         for(int i=0 ; i<n-1 ;i++){
             for(int cur = v[i]+1 ; cur<v[i+1] ; ++cur){
                 ans.push_back(cur);
