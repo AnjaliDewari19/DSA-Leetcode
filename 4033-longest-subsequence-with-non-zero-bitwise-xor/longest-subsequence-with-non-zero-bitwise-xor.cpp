8c511023174ef5cb54b59f5794315d46aacc0654
Anjali Dewari <anjalidewari19@gmail.com>
2026-08-15 20:49:07 +0530

    Longest Subsequence  With Non Zero Bitwise Xor M

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -10,17 +10,14 @@ public:
                 hasNonZero = true;
             }
         }
-
         // If all elements are zero, no subsequence can have non-zero XOR
         if (!hasNonZero) {
             return 0;
         }
-
         // If total XOR is already non-zero, take the whole array
         if (totalXor != 0) {
             return nums.size();
         }
-
         // Otherwise, exclude any one non-zero element to get non-zero XOR
         return nums.size() - 1;
     }
