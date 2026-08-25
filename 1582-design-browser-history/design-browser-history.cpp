8c2ff1042ec13e1c49092f6314fcf851518336bb
Anjali Dewari <anjalidewari19@gmail.com>
2026-08-25 15:44:12 +0530

    Design browser history M

--- 1582-design-browser-history/design-browser-history.cpp
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -35,11 +35,3 @@ public:
         return curr;
     }
 };
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory* obj = new BrowserHistory(homepage);
- * obj->visit(url);
- * string param_2 = obj->back(steps);
- * string param_3 = obj->forward(steps);
- */
