b7cae033e5bff97af3a35072a7271165df39d339
Anjali Dewari <anjalidewari19@gmail.com>
2026-08-29 15:31:43 +0530

    Time: 129 ms (89.30%) | Memory: 140.6 MB (89.86%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<pair<int, int>> paired(n);
+        for (int i = 0; i < n; ++i) {
+            paired[i] = {nums[i], i};
+        }
+
+        // Sort pairs by value
+        sort(paired.begin(), paired.end());
+
+        vector<int> ans(n);
+        int i = 0;
+
+        while (i < n) {
+            int j = i;
+            while (j + 1 < n && paired[j + 1].first - paired[j].first <= limit) {
+                j++;
+            }
+
+            vector<int> indices;
+            for (int k = i; k <= j; ++k) {
+                indices.push_back(paired[k].second);
+            }
+
+            sort(indices.begin(), indices.end());
+
+            for (int k = i; k <= j; ++k) {
+                ans[indices[k - i]] = paired[k].first;
+            }
+
+            i = j + 1;
+        }
+
+        return ans;
+    }
+};
