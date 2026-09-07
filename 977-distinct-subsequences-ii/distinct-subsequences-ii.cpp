29a3fa7147cf3f81f14e0d2684b05b504405567b
Anjali Dewari <anjalidewari19@gmail.com>
2026-09-07 19:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (78.81%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        vector<long> ends_with(26,0);
+        int mod = 1e9 + 7;
+
+        for(char c : s){
+            long total = 0;
+            for(int count : ends_with){
+                total = (total + count) % mod;
+            }
+            ends_with[c - 'a'] = (total+1) % mod;
+        }
+        long result = 0;
+        for(int count : ends_with){
+            result = (result + count) % mod;
+        }
+
+        return result;
+    }
+};
