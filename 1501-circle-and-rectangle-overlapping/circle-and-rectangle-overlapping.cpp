3c8cb0ad78942b29f46a0790f281aa7d1a93141d
Anjali Dewari <anjalidewari19@gmail.com>
2026-09-19 20:36:39 +0530

    Circle and rectangle overlapping M

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -3,6 +3,7 @@ public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
         int closestX = max(x1, min(xCenter, x2));
         int closestY = max(y1, min(yCenter, y2));
+        
         int dx = xCenter - closestX;
         int dy = yCenter - closestY;
 
